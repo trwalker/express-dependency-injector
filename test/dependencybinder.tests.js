c12fd52fd18ecc12565918be42811d3c8ddac1ad
Tim Walker <trwalker@godaddy.com>
2014-08-12 06:48:39 -0700

    fixed singleton typo in thrown exceptions

--- test/dependencybinder.tests.js
+++ test/dependencybinder.tests.js
@@ -43,28 +43,28 @@ describe('DependencyBinder Tests', function() {
       var bindFunction = dependencyBinder.bind;
       var scope = null;
 
-      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should throw on undefined scope', function() {
       var bindFunction = dependencyBinder.bind;
       var scope;
 
-      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should throw on empty scope', function() {
       var bindFunction = dependencyBinder.bind;
       var scope = '';
 
-      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should throw on bad scope', function() {
       var bindFunction = dependencyBinder.bind;
       var scope = 'scopedoesnotexist';
 
-      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Invalid dependency scope. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(bindFunction.bind(dependencyBinder, MyClass, [MyDependency], scope)).to.throw('Invalid dependency scope. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should add item with singleton scope', function() {
@@ -125,28 +125,28 @@ describe('DependencyBinder Tests', function() {
       var getFunction = dependencyBinder.get;
       var scope = null;
 
-      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should throw on undefined scope', function() {
       var getFunction = dependencyBinder.get;
       var scope;
 
-      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should throw on empty scope', function() {
       var getFunction = dependencyBinder.get;
       var scope = '';
 
-      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Dependency scope cannot be null or undefined. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should throw on bad scope', function() {
       var getFunction = dependencyBinder.get;
       var scope = 'scopedoesnotexist';
 
-      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Invalid dependency scope. Valid scope types are "signleton", "webrequest", and "transient".');
+      expect(getFunction.bind(dependencyBinder, scope)).to.throw('Invalid dependency scope. Valid scope types are "singleton", "webrequest", and "transient".');
     });
 
     it('should return empty items on valid scope', function() {
